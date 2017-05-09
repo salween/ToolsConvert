52d940ed8b0b6044e542629f72828a2ed147872a
DESKTOP-O678LRJ\MY-PC <nan@salweensolutions.com>
2017-05-09 09:30:03 +0700

    fix project bulid structured.

---
+++ ConvertCSVToXmlOrchard/ConvertCSVToXmlOrchard/Model/CSVModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertCSVToXmlOrchard
+{
+    class CSVModel
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+    }
+}
